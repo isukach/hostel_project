4dcba28ded6c2a56ec9ef07104c33bdba4707cc6
KeFA <tokefa@gmail.com>
2011-01-12 21:50:12 +0200

    sdf

--- src/main/sql/hostelBackup 20100821 1629.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `app_user` (`id`,`account_expired`,`account_locked`,`address`,`city`,`country`,`postal_code`,`province`,`credentials_expired`,`email`,`account_enabled`,`first_name`,`last_name`,`password`,`password_hint`,`phone_number`,`username`,`version`,`website`) VALUES 
-
- (5,0x00,0x00,'Y.Kolasa 28','Minsk','BY','777777','Minsk',0x00,'to_adminkoasd@tut.by',0x01,'adminko','kefa','62832393ae85b23d37566095fba817f7c4bcd908','=)','','kefa',0,'');
- 
-
-INSERT INTO `user_role` (`user_id`,`role_id`) VALUES 
- (2,-1),
- (3,-1);
-
