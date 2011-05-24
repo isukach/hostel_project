f085243a361571b26588803ecd34a97a1364c84d
KeFA <tokefa@gmail.com>
2011-05-24 10:25:09 +0300

    issue #79 - users on users list page for floorhead now are sorted by room

--- src/main/java/war/webapp/action/UserList.java
+++ src/main/java/war/webapp/action/UserList.java
@@ -6,6 +6,8 @@ import war.webapp.model.User;
 import war.webapp.util.FacesUtils;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class UserList extends BasePage implements Serializable {
@@ -17,16 +19,24 @@ public class UserList extends BasePage implements Serializable {
     }
 
     public List<User> getUsers() {
-        return (List<User>) sort(userManager.getUsers());
+        return sortByRoom(userManager.getUsers());
     }
 
     public List<User> getUsersByFloorheadFloor() {
-        User user = (User) ((SecurityContext) FacesUtils.getSession().getAttribute(
-                HttpSessionContextIntegrationFilter.SPRING_SECURITY_CONTEXT_KEY)).getAuthentication().getPrincipal();
+        User user = userManager.getUserByUsername(getRequest().getRemoteUser());
         String floor = user.getAddress().getHostelFloor();
         List<User> floorUsers = userManager.getUsersByFloor(floor);
         floorUsers.remove(user);
-        return (List<User>) sort(floorUsers);
+        return sortByRoom(floorUsers);
+    }
+
+    private List<User> sortByRoom(List<User> users) {
+        Collections.sort(users, new Comparator<User>() {
+            public int compare(User u1, User u2) {
+                return u1.getAddress().getHostelRoom().compareToIgnoreCase(u2.getAddress().getHostelRoom());
+            }
+        });
+        return users;
     }
 
     public String getCurrentUserFloor() {
